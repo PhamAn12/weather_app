7fd4fba98effc4d282816f6483497d5710d06b11
phamkhacan1903 <phamkhacan1903@gmail.com>
2019-03-06 11:27:16 +0700

    add manhinh

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,13 +1,25 @@
+// API key: 94f7d25e8b529c9034a92a47e021663c
 package com.example.weather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btnMove);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,ManageLocation.class);
+                startActivity(intent);
+            }
+        });
     }
 }
